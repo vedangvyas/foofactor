329d731ff320c07d2160d2d077482fc8e5c35a07
csiu <celia.sy.siu@gmail.com>
2015-11-19 14:50:05 -0800

    made it so that freq_out has 3 unit tests

--- R/freq_out.R
+++ R/freq_out.R
@@ -1,12 +1,26 @@
 #' Create a frequency table for a factor
 #'
 #' @param x factor
+#' @param n.sort one of \code{c(NULL, "ascending", "descending")}. This argument specifies how to sort the counts in the frequency table.
 #'
 #' @return tbl_df
 #' @export
 #' @examples
 #' freq_out(iris$Species)
-freq_out <- function(x) {
+freq_out <- function(x, n.sort=NULL) {
   xdf <- dplyr::data_frame(x)
-  dplyr::count(xdf, x)
+  xdf <- dplyr::count(xdf, x)
+
+  if (!is.null(n.sort)) {
+    if (n.sort == "ascending") {
+      dplyr::arrange(xdf, n)
+    } else if (n.sort == "descending") {
+      dplyr::arrange(xdf, desc(n))
+    } else {
+      warning(sprintf("'%s' is not a sort option", n.sort))
+      xdf
+    }
+  } else {
+    xdf
+  }
 }
--- tests/testthat/test_freq_out.R
+++ tests/testthat/test_freq_out.R
@@ -8,3 +8,20 @@ test_that("freq_out create a frequency table for a factor", {
 
   expect_identical(res, e)
 })
+
+test_that("freq_out will sort output by ascending and descending order", {
+  x <- factor(c(rep("three", 3), rep("two", 2), rep("five", 5)))
+
+  expect_equal(as.character(freq_out(x, n.sort = NULL)$x),
+               as.character(levels(x)))
+
+  expect_equal(as.character(freq_out(x, n.sort = "ascending")$x),
+               c("two", "three", "five"))
+  expect_equal(as.character(freq_out(x, n.sort = "descending")$x),
+               c("five", "three", "two"))
+})
+
+test_that("freq_out will give a warning if n.sort is not one of the options", {
+  expect_warning(freq_out(iris$Species, n.sort="fake"),
+                 "'fake' is not a sort option")
+})
