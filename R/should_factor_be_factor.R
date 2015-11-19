f0399016919f688720f1931cb24d2a855ae3a83c
csiu <celia.sy.siu@gmail.com>
2015-11-19 15:55:03 -0800

    update example

--- R/should_factor_be_factor.R
+++ R/should_factor_be_factor.R
@@ -5,11 +5,14 @@
 #' @return boolean; \code{TRUE} if the number of unique values does not equal to the length, and \code{FALSE} otherwise.
 #' @export
 #' @examples
+#' abb <- factor(c("a", "b", "b"))
+#' abc <- factor(c("a", "b", "c"))
+#'
 #' # This should be a factor:
-#' should_factor_be_factor(factor(c("a", "b", "b")))
+#' should_factor_be_factor(abb)
 #'
 #' # This should not be a factor
-#' should_factor_be_factor(factor(c("a", "b", "c")))
+#' should_factor_be_factor(abc)
 should_factor_be_factor <- function(x){
   if (is.factor(x))
     length(x) != length(unique(x))
