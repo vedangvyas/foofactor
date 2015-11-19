1c089aaaf220720820a0d0c2f41cea33cdac0b31
csiu <celia.sy.siu@gmail.com>
2015-11-19 13:30:56 -0800

    modify fbind to add additional functionality with sensible default argument

--- R/fbind.R
+++ R/fbind.R
@@ -2,11 +2,13 @@
 #'
 #' @param a factor
 #' @param b factor
+#' @param level.as.appears boolean; should the factor levels be combined as they appear?
 #'
 #' @return factor
 #' @export
 #' @examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
-fbind <- function(a, b){
-  factor(c(as.character(a), as.character(b)))
+fbind <- function(a, b, level.as.appears=TRUE){
+  ab <- factor(c(as.character(a), as.character(b)))
+  factor(ab, levels = c(levels(a), levels(b)))
 }
