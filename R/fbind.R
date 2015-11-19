a8c42bd2ced9e0bd3d8ec547f8c0940f068a50dc
csiu <celia.sy.siu@gmail.com>
2015-11-19 13:50:37 -0800

    fbind failed check ... must correct bug

--- R/fbind.R
+++ R/fbind.R
@@ -10,5 +10,5 @@
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b, level.as.appears=TRUE){
   ab <- factor(c(as.character(a), as.character(b)))
-  factor(ab, levels = c(levels(a), levels(b)))
+  if (level.as.appears) factor(ab, levels = c(levels(a), levels(b))) else ab
 }
--- tests/testthat/test_fbind.R
+++ tests/testthat/test_fbind.R
@@ -1,8 +1,8 @@
 test_that("fbind binds factor", {
   a <- iris$Species[c(1, 51, 101)]
   b <- PlantGrowth$group[c(1, 11, 21)]
-  res <- fbind(a, b)
-  expect_identical(res,
+  res <- fbind(a, b, level.as.appears = FALSE)
+  expect_equivalent(res,
                    factor(c(levels(iris$Species),
                             levels(PlantGrowth$group))))
 })
