0faa155c30f084287fee3bc9f305e4d811ee5aac
csiu <celia.sy.siu@gmail.com>
2015-11-19 14:49:38 -0800

    made it so that fbind has 3 unit tests

--- tests/testthat/test_fbind.R
+++ tests/testthat/test_fbind.R
@@ -14,3 +14,9 @@ test_that("fbind binds factor levels as they appear", {
   expect_equivalent(res,
                     c(levels(a), levels(b)))
 })
+
+test_that("fbind results to NA if it tries to bind non factors", {
+  res <- fbind("a", "b")
+  expect_identical(res,
+                   as.factor(c(NA, NA)))
+})
