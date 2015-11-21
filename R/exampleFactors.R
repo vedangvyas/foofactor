eb74598c7f7863d312c86a0735877d599904d104
csiu <celia.sy.siu@gmail.com>
2015-11-20 19:57:17 -0800

    need to doc example data

---
+++ R/exampleFactors.R
@@ -0,0 +1,19 @@
+#' Example factor 1
+#'
+#' @format A factor with 3 levels of length 3
+#' @details Alias for \code{iris$Species[c(1, 51, 101)]}
+"exampleFactor1"
+
+
+#' Example factor 2
+#'
+#' @format A factor with 3 levels of length 3
+#' @details Alias for \code{PlantGrowth$group[c(1, 11, 21)]}
+"exampleFactor2"
+
+
+#' Example factor 3
+#'
+#' @format A factor with 3 levels of length 34
+#' @details Alias for \code{iris$Species[c(1:10, 51:65, 101:109)]}
+"exampleFactor3"
